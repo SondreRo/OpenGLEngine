9bc57ea1c4c32a63c28be65487c05f97c7d7de92
Sondre R <soroensg@gmail.com>
2024-09-05 11:30:35 +0200

    Added Sphere

--- out/build/x64-debug/cmake_install.cmake
+++ out/build/x64-debug/cmake_install.cmake
@@ -32,6 +32,13 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("D:/School/3DProg/MyProgrmaming/MainPrograms/OpenGL/CMakeMine/OpenGLEngine/out/build/x64-debug/libs/glfw-3.3.9/cmake_install.cmake")
+  include("D:/School/3DProg/MyProgrmaming/MainPrograms/OpenGL/CMakeMine/OpenGLEngine/out/build/x64-debug/libs/assimp/cmake_install.cmake")
+
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
